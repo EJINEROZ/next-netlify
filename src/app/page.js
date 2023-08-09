66bcd6fd75d799b79426967d3c4670164e2b8eec
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-09 20:57:30 +0100

    test3

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
      <div className="bg-white p-8 rounded-lg shadow-md w-96">
     <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
-    <form name="contact" method="post" netlify>
+    <form name="contact" method="post">
       <div className="mb-4">
         <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
         <input type="text" id="name" name="name" className="mt-1 p-2 border rounded-md w-full focus:ring focus:ring-blue-300" />
