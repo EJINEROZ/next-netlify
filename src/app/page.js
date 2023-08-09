4f0dcbc7baa6ce4e93e60ce0c22cc7e0d9db76bb
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-09 19:58:43 +0100

    test2

--- src/app/page.js
+++ src/app/page.js
@@ -2,36 +2,8 @@
 import Image from 'next/image'
 
 export default function Home() {
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    // const data = {
-    //   name: String(event.target.name.value),
-    //   email: String(event.target.email.value),
-    //   message: String(event.target.message.value),
-
-    // };
-    // console.log(data);
-  }
   return (
-
      <div className="bg-white p-8 rounded-lg shadow-md w-96">
-    {/* <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form> */}
-
     <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
     <form>
       <div className="mb-4">
